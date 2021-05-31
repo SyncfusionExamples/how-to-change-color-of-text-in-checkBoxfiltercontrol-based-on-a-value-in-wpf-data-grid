48aa68f51958ace6691d283aa28da4e49dea4936
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2021-05-31 21:38:25 +0530

    Sample Added
    Sample Added

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SfDataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //Get the CheckBox
+            var checBox = value as CheckBox;
+
+            //Get the content of CheckBox 
+            var record = checBox.Content.ToString();
+
+            //You can use a BrushConverter
+            SolidColorBrush redBrush = (SolidColorBrush)new BrushConverter().ConvertFromString(record);
+
+            //return the color based on DataBase text in GridFilterControl 
+            return redBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
